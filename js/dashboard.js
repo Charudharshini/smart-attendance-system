4d8dd43afbd8633f3f1945611cbd93971ac3c842
Your Name <charu06112001@gmail.com>
2021-11-15 22:59:49 +0530

    add student dashboard

---
+++ js/dashboard.js
@@ -0,0 +1,19 @@
+function onclickForstudentinbox() {
+    console.log("Studentinbox button clicked");
+    location.href= "studentinbox.html";
+}
+
+function onclickForstudentleaveletter() {
+    console.log("Admin button clicked");
+    location.href= "admin.html";
+}
+
+function onclickForattendancestatus() {
+    console.log("Faculty button clicked");
+    location.href= "faculty.html";
+}
+
+function onclickForattendancecalendar() {
+    console.log("Faculty button clicked");
+    location.href= "faculty.html";
+}
